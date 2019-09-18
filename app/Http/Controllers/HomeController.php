d28da616a2bca9b1a7516c0cc1b4b1297a5ef5cc
Uki7991 <tilek.kubanov@gmail.com>
2019-09-18 21:16:05 +0600

    parser

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
+use PHPHtmlParser\Dom;
 
 class HomeController extends Controller
 {
@@ -21,8 +23,29 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('home');
+        $page = 1;
+        if ($request->page) {
+            $page = $request->page;
+        }
+        $url = 'http://ed.kyrg.info/category/news-events/page/'.$page.'/';
+        $dom = new Dom();
+        $dom->load($url);
+        $h2 = $dom->find('h2.entry-title > a');
+        $excerpt = $dom->find('div#contentColumn > p');
+        $posts = [];
+        if (count($h2) == count($excerpt)) {
+            for ($i = 0; $i < count($h2); $i++) {
+                $post = new Post();
+                $post->title = $h2[$i]->text;
+                $post->link = $h2[$i]->href;
+                $post->excerpt = $excerpt[$i]->text;
+                $posts[] = $post;
+            }
+        }
+        return view('home', [
+            'posts' => $posts,
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,20 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+        @foreach($posts as $post)
+            <div class="col-8 mb-3">
+                <div class="card">
+                    <div class="card-header">
+                        <a href="{{ $post->link }}">
+                            {{ $post->title }}
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        {!! $post->excerpt !!}
+                    </div>
                 </div>
             </div>
-        </div>
+        @endforeach
     </div>
 </div>
 @endsection
