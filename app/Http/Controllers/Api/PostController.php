8d783d745102c0508cf82afee88671bf2f4eb0f0
Uki7991 <tilek.kubanov@gmail.com>
2019-09-19 12:06:17 +0600

    api

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Post;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use PHPHtmlParser\Dom;
+
+class PostController extends Controller
+{
+    public function index(Request $request)
+    {
+        $url = 'http://ed.kyrg.info/category/news-events';
+        if ($request->page) {
+            $url = $request->page;
+        } else {
+            $url = 'http://ed.kyrg.info/category/news-events/';
+        }
+        $dom = new Dom();
+        $dom->load($url);
+        $h2 = $dom->find('h2.entry-title > a');
+        $excerpt = $dom->find('div#contentColumn > p');
+        $prev = $dom->find('div.nav-previous > a');
+        $next = $dom->find('div.nav-next > a');
+        $posts = [];
+        if (count($h2) == count($excerpt)) {
+            for ($i = 0; $i < count($h2); $i++) {
+                $post = new Post();
+                $post->title = $h2[$i]->text;
+                $post->link = $h2[$i]->href;
+                $post->excerpt = $excerpt[$i]->text;
+                $posts[] = $post;
+            }
+        }
+        return response()->json([
+            'posts' => $posts,
+            'prev' => isset($prev[0]) ? $prev[0]->href : null,
+            'next' => isset($next[0]) ? $next[0]->href : null,
+        ]);
+    }
+
+    public function show(Request $request)
+    {
+        $dom = new Dom();
+        $dom->load($request->link);
+        $h1 = $dom->find('h1.entry-title');
+        $contentHtml = $dom->find('div.entry-content > p');
+        $content = '';
+        foreach ($contentHtml as $item) {
+            $content .= $item->outerHtml;
+            $content .= ' ';
+        }
+        $title = $h1[0]->text;
+        $post = new Post();
+        $post->title = $title;
+        $post->content = $content;
+        return response()->json([
+            'post' => $post,
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/posts', 'Api\PostController@index');
+Route::get('/posts/show', 'Api\PostController@show');
