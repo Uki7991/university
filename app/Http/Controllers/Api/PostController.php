e0687646957b803ed395dc8ae2702c7e79cf0db7
Uki7991 <tilek.kubanov@gmail.com>
2019-12-04 21:15:46 +0600

    Update PostController.php

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -17,23 +17,29 @@ class PostController extends Controller
         } else {
             $url = 'http://ed.kyrg.info/category/news-events/';
         }
+
+        return $this->returnView($url, 'news');
+    }
+
+    public function returnView($url, $type)
+    {
         $dom = new Dom();
         $dom->load($url);
-        $h2 = $dom->find('h2.entry-title > a');
-        $excerpt = $dom->find('div#contentColumn > p');
+        $postsDom = $dom->find('.post_div');
         $prev = $dom->find('div.nav-previous > a');
         $next = $dom->find('div.nav-next > a');
         $countPosts = $dom->find('#post_count_t')->value;
-        $date = $dom->find('.entry-meta > b');
         $posts = [];
-            for ($i = 0; $i < count($h2); $i++) {
-                $post = new Post();
-                $post->title = $h2[$i]->text;
-                $post->link = $h2[$i]->href;
-                $post->excerpt = $excerpt[$i]? $excerpt[$i]->text : '';
-                $post->date = $date[$i] ? $date[$i]->text : '';
-                $posts[] = $post;
-            }
+        for ($i = 0; $i < count($postsDom); $i++) {
+            $post = new Post();
+            $post->title = $postsDom[$i]->find('h2.entry-title > a')[0]->text;
+            $post->link = $postsDom[$i]->find('h2.entry-title > a')[0]->href;
+            $post->excerpt = $postsDom[$i]->find('p')[0] ? $postsDom[$i]->find('p')[0]->text : '';
+            $post->date = $postsDom[$i]->find('.entry-meta > b')[0] ? $postsDom[$i]->find('.entry-meta > b')[0]->text : '';
+            $post->image = $postsDom[$i]->find('img.wp-post-image')[0] ? $postsDom[$i]->find('img.wp-post-image')[0]->src : '';
+            $post->type = $type;
+            $posts[] = $post;
+        }
         return response()->json([
             'posts' => $posts,
             'prev' => isset($prev[0]) ? $prev[0]->href : null,
@@ -70,29 +76,8 @@ class PostController extends Controller
         } else {
             $url = 'http://ed.kyrg.info/category/announcements/';
         }
-        $dom = new Dom();
-        $dom->load($url);
-        $h2 = $dom->find('h2.entry-title > a');
-        $excerpt = $dom->find('div#contentColumn > p');
-        $prev = $dom->find('div.nav-previous > a');
-        $next = $dom->find('div.nav-next > a');
-        $countPosts = $dom->find('#post_count_t')->value;
-        $date = $dom->find('.entry-meta > b');
-        $posts = [];
-            for ($i = 0; $i < count($h2); $i++) {
-                $post = new Post();
-                $post->title = $h2[$i]->text;
-                $post->link = $h2[$i]->href;
-                $post->excerpt = $excerpt[$i] ? $excerpt[$i]->text : '';
-                $post->date = $date[$i] ? $date[$i]->text : '';
-                $posts[] = $post;
-            }
-        return response()->json([
-            'posts' => $posts,
-            'prev' => isset($prev[0]) ? $prev[0]->href : null,
-            'next' => isset($next[0]) ? $next[0]->href : null,
-            'countPosts' => $countPosts,
-        ]);
+
+        return $this->returnView($url, 'announcments');
     }
 
     public function show2(Request $request)
